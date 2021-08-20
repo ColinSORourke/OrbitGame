1d413faf9efc3a46e9a33096b03150d551f08bf0
ColinSORourke <csorourk@ucsc.edu>
2021-08-20 11:24:39 -0700

    Also this

--- src/scenes/preload.js
+++ src/scenes/preload.js
@@ -8,6 +8,8 @@ class Preload extends Phaser.Scene
     {   
         this.load.image('Head', './assets/ColinHead.jpg');
         this.load.image('Sun', './assets/kenney_simplespace/PNG/Retina/meteor_squareLarge.png')
+        this.load.image('Comet', './assets/kenney_simplespace/PNG/Retina/meteor_squareDetailedSmall.png')
+        this.load.image('Ship', './assets/kenney_simplespace/PNG/Retina/ship_A.png')
 
         this.createProgressbar(game.config.width / 2, game.config.height / 2);
     }
